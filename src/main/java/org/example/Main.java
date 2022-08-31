e6c5f2ff11ec16e095563a637d26c9d6d08b6e8e
lp <lp@7017.com>
2022-08-31 17:33:51 +0800

    hot-fix commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!333");
         System.out.println("Hello world!333");
+        System.out.println("hot-fix  test!");
     }
 }
